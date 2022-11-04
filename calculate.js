5ae74303d930f5a7a4f8ac35977023088ec51582
2emk7 <104653877+2emk7@users.noreply.github.com>
2022-11-04 10:30:48 -0400

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -6,4 +6,4 @@ var s1 = 9.8 * height;
 var s2 = s1 * 2;
 var s3 = Math.sqrt(s2);
 
-prompt("If you jump off a building " + height + "meters tall, your impact velocity will be "  + s3 + " m/s");
+alert("If you jump off a building " + height + "meters tall, your impact velocity will be "  + s3 + " m/s");
