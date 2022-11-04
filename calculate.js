982acf4b8da969f848db212ad1d43638e396a9a3
2emk7 <104653877+2emk7@users.noreply.github.com>
2022-11-03 23:56:46 -0400

    Create calculate.js

---
+++ calculate.js
@@ -0,0 +1,8 @@
+var height = prompt("What height are you jumping from (meters)? ");
+var weight = prompt("What is your weight (in kg)?");
+
+var s1 = 9.8 * height;
+var s2 = s1 * 2;
+var s3 = Math.sqrt(s2);
+
+System.out.println("If you jump off a building " + height + "meters tall, your impact velocity will be "  + s3 + " m/s");
