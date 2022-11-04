e35d841473ba0c215c9b17b78191b73892643748
2emk7 <104653877+2emk7@users.noreply.github.com>
2022-11-04 13:26:33 -0400

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,9 +1,9 @@
 javascript:
 var height = prompt("What height are you jumping from? ");
 
-height = height/3.281; 
+height = Math.round(height/3.281);
 var s1 = 9.8 * height;
 var s2 = s1 * 2;
 var s3 = Math.sqrt(s2);
 
-alert("If you jump off a building " + height + " meters tall, your impact velocity will be "  + s3 + " m/s");
+alert("If you jump off a building " + height + " meters tall, your impact velocity will be "  + Math.round(s3) + " m/s");
